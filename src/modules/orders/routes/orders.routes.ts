4ba3071cfa0787e103cb4d2c2a1c8dcac759b8a4
guilhermetwelve0 <guilherme.cabral999@hotmail.com>
2021-09-14 09:13:30 -0300

    segundo commit

--- src/modules/orders/routes/orders.routes.ts
+++ src/modules/orders/routes/orders.routes.ts
@@ -2,9 +2,12 @@ import { Router } from 'express';
 
 import { celebrate, Joi, Segments } from 'celebrate';
 import OrdersController from '../controllers/OrdersController';
+import isAuthenticated from '@shared/http/middlewares/isAuthenticated';
 
 const ordersRouter = Router();
 const ordersController = new OrdersController();
+
+ordersRouter.use(isAuthenticated);
 
 ordersRouter.get(
   '/:id',
